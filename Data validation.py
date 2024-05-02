3199fcc629f4f7e6483bcceb67c2db4ddb4e74ad
Practical2001 <144357246+Practical2001@users.noreply.github.com>
2024-05-02 15:56:44 +0100

    Add files via upload

---
+++ Data validation.py
@@ -0,0 +1,22 @@
+#Data validation
+def lengthcheck(word, length, option):
+    if option ==1:
+        if len(word)==length:
+           return True
+    else:
+    return False
+
+# option 2 checking if the word has length not shorter than lentgh
+
+     elif option == 2:
+
+
+if len(word) >= length:
+    return True else:
+    return False
+
+             if len(word) >= length: return True else: return False
+
+# option 3 checking if the word has length not longer than length
+
+elif option == 3: if len(word)<= length: return True else: return False
